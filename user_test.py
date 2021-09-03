c28c0a5e43054493caf24a6739a6006a1a8b1659
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-03 12:07:17 +0300

    pushing a test method for testing if the app is saving generated objects

--- user_test.py
+++ user_test.py
@@ -18,5 +18,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username, "Vitalis")
         self.assertEqual(self.new_user.password, "ptalz#75")
 
+    def test_save_user(self):
+        """
+        A test method to test if our app is saving the objects/users.
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list), 1)
+
 if __name__ == '__main__':
     unittest.main()
