4d540f6026e82e79d38104e873c0b9a4d8771a8a
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-03 21:53:30 +0300

    pushing user options on password creation

--- run.py
+++ run.py
@@ -10,63 +10,74 @@ def create_user(username, password):
     new_user = User(username, password)
     return new_user
 
-    def save_user(user):
-        """
-        Function for saving user.
-        """
-        user.save_user()
+def save_user_main(user):
+    """
+    Function for saving user.
+    """
+    user.save_user()
 
-    def find_user(username):
-        """
-        Function to find user by username and returns user's credentials.
-        """
-        return User.find_user(username)
+def find_user_main(username):
+    """
+    Function to find user by username and returns user's credentials.
+    """
+    return User.find_user(username)
 
-    def check_existing_users(username):
-        """
-        Function to check users exists using their usernames
-        """
-        return User.users_exists(username)
+def check_existing_users(username):
+    """
+    Function to check users exists using their usernames
+    """
+    return User.users_exists(username)
 
-    #End of user class functions
+#End of user class functions
 
-    #Function for Credentials class
+#Function for Credentials class
 
-    def create_account(account_type, username, password):
-        """
-        Function that helps users create account.
-        """
-        new_account = Credentials(account_type, username, password)
-        return new_account
+def create_account(account_type, username, password):
+    """
+    Function that helps users create account.
+    """
+    new_account = Credentials(account_type, username, password)
+    return new_account
 
-    def save_account(credentials):
-        """
-        Function for saving the new account credentials
-        """
-        credentials.save_credentials()
+def save_account(credentials):
+    """
+    Function for saving the new account credentials
+    """
+    credentials.save_credentials()
 
-    def find_account(account_type):
-        """
-        Function for finding using the type of account name.
-        """
-        return Credentials.find_credentials(account_type)
+def find_account(account_type):
+    """
+    Function for finding using the type of account name.
+    """
+    return Credentials.find_credentials(account_type)
 
-    def check_existance(account_type):
-        """
-        Function for checking if an account exists using account tpe name.
-        """
-        return Credentials.credentials_exists(account_type)
+def check_existance(account_type):
+    """
+    Function for checking if an account exists using account tpe name.
+    """
+    return Credentials.credentials_exists(account_type)
 
-    def delete_account(credentials):
-        """
-        Function deleting account
-        """
-        credentials.delete_credentials()
-        
-    def display_account():
-        """
-        Function that returns all saved account credentials.
-        """
-        return Credentials.display_credentials()
+def delete_account(credentials):
+    """
+    Function deleting account
+    """
+    credentials.delete_credentials()
+    
+def display_account():
+    """
+    Function that returns all saved account credentials.
+    """
+    return Credentials.display_credentials()
 
-    #End of credential class functions
+#End of credential class functions
+
+#Password creation choice
+
+def password_choice(quiz):
+    """
+    Function that defines mode of password creation.
+    """
+    user_choice = None
+    while user_choice not in ("yes", "no"):
+        user_choice = input(quiz).lower()
+    return user_choice
