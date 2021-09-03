19b1872d339959c5ca82daaeafa7d1287b6d1afc
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-03 11:51:33 +0300

    pushing init test method.Created a setup method too.

--- user_test.py
+++ user_test.py
@@ -5,3 +5,18 @@ class TestUser(unittest.TestCase):
     """
     Test class that defines test cases for out user class.
     """
+    def setUp(self):
+        """
+        Setup method that is run before each test case
+        """
+        self.new_user = User("Vitalis", "ptalz#75")
+
+    def test__init__(self):
+        """
+        Test init method to test if objects properties are being initialized properly
+        """
+        self.assertEqual(self.new_user.username, "Vitalis")
+        self.assertEqual(self.new_user.password, "ptalz#75")
+
+if __name__ == '__main__':
+    unittest.main()
